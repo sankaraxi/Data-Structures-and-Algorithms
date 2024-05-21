85eaee80dc61f8eb873dcbf9661865672f0c6c39
sankaraxi <sankarfrompalani@gmail.com>
2024-05-21 23:31:01 +0530

    Learned Bubble Sort

---
+++ DSA/Arrarys/BubbleSortJava/src/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr ={9,4,2,5,1,7,3,0,6,8};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void bubbleSort(int[] arr){
+        boolean swapped;
+        //runs the steps n - 1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            //for each step max item will come at the last respective index
+            for (int j = 1; j < arr.length-i; j++) {
+                // swap if the item is smaller than the previous item
+                if (arr[j] < arr[j-1]){
+                    //swap
+                     int temp = arr[j];
+                     arr[j] = arr[j-1];
+                     arr[j-1] = temp;
+                     swapped = true;
+                }
+            }
+            //if you did not swap for a particular value of i, it means the array is sorted hence stop the program
+            if (!swapped){
+                break;
+            }
+        }
+    }
+}
