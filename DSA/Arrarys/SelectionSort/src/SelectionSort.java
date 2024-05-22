a09ab892c3c94200c465c6fbb2249ac6d53ac43f
sankaraxi <sankarfrompalani@gmail.com>
2024-05-22 23:50:20 +0530

    Learned Selection Sort

---
+++ DSA/Arrarys/SelectionSort/src/SelectionSort.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SelectionSort {
+    public static void main(String[] args) {
+
+    }
+    static void selectionSort(int[] arr){
+        System.out.println("Selection Sort");
+    }
+}
