caca6ee2c0ace3e7182d135b977a21b62703d506
sankaraxi <sankarfrompalani@gmail.com>
2024-05-23 17:57:27 +0530

    Learned Selection Sort

--- DSA/Arrarys/SelectionSort/src/SelectionSort.java
+++ DSA/Arrarys/SelectionSort/src/SelectionSort.java
@@ -1,10 +1,35 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SelectionSort {
     public static void main(String[] args) {
-
+        int[] arr ={9,4,2,5,1,7,3,0,6,8};
+        System.out.println(Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
     static void selectionSort(int[] arr){
-        System.out.println("Selection Sort");
+        for (int i = 0; i < arr.length; i++) {
+            //find the maximum item in the remaining array and swap with the correct item
+            int last = arr.length -i -1;
+            int maxIndex = getMaxIndex(arr,0,last);
+            swap(arr,maxIndex,last);
+        }
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i]>arr[max]){
+                max=i;
+            }
+        }
+        return max;
+    }
+    static void swap (int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
     }
 }
