49bb7f1f6358e7f7cbc67eedb1a2a8905339adaf
sankaraxi <sankarfrompalani@gmail.com>
2024-06-16 13:56:20 +0530

    Skip A Character and Skip a String

--- DSA/Recursion/ProblemsRecursion/StringsREC/src/SkipAString.java
+++ DSA/Recursion/ProblemsRecursion/StringsREC/src/SkipAString.java
@@ -1,3 +1,18 @@
 public class SkipAString {
+    public static void main(String[] args) {
+        String str = "bsddapplefsgg";
+        System.out.println(skipApple(str));
+    }
 
+    static String skipApple(String str){
+        if (str.isEmpty()){
+            return "";
+        }
+
+        if(str.startsWith("apple")){
+            return skipApple(str.substring(5));
+        }else{
+           return str.charAt(0) + skipApple(str.substring(1));
+        }
+    }
 }
