cd881c547ae687eb4a4070769d9489788e5e7bae
sankaraxi <sankarfrompalani@gmail.com>
2024-06-18 23:48:07 +0530

    Learned about packages

---
+++ OOP/OOP Intro/src/packages/c/Message.java
@@ -0,0 +1,11 @@
+package packages.c;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message(){
+        System.out.println("This is a message displayed from package 'c' ");
+    }
+}
