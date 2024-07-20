05c34619431b5b3166eb928f20d0c4ce7ec6210f
sankaraxi <sankarfrompalani@gmail.com>
2024-07-20 12:14:28 +0530

    revised bubbled sort

--- DSA/Arrarys/BubbleSortJava/src/BubbleSort.java
+++ DSA/Arrarys/BubbleSortJava/src/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
         boolean swapped;
         //runs the steps n - 1 times
         for (int i = 0; i < arr.length; i++) {
-            swapped = false;
+            swapped = false; //optimization which will help to stop the program if the array is already sorted
             //for each step max item will come at the last respective index
             for (int j = 1; j < arr.length-i; j++) {
                 // swap if the item is smaller than the previous item
