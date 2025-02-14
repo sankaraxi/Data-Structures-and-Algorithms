7d3af014f11882afd8f0c6f5b11ce2b5766fec97
Sankar K G <sankarfrompalani@gmail.com>
2025-02-14 23:46:50 +0530

    solved countgood numbers

--- DSA/Recursion/src/basicProblems/CountGoodNumbers.java
+++ DSA/Recursion/src/basicProblems/CountGoodNumbers.java
@@ -1,7 +1,15 @@
 package basicProblems;
 
 public class CountGoodNumbers {
+    // 1922. Count Good Numbers
     public static void main(String[] args) {
+        System.out.println(countGoodNumbers(50));
+    }
 
+    public static int countGoodNumbers(long n) {
+        long mod = 1000000007;
+        long odd = (n + 1) / 2;
+        long even = n / 2;
+        return (int) (Math.pow(5, odd, mod) * Math.pow(4, even, mod) % mod);
     }
 }
